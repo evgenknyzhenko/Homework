2db01ab78bd1cd0ee1fe479611011be71c995573
Evgen <evgen.knyzhenko@gmail.com>
2018-05-08 10:44:34 +0300

    Homework01_1

--- src/Homework.java
+++ src/Homework.java
@@ -7,12 +7,10 @@ import java.util.Scanner;
 public class Homework {
     public static void main(String[] args) throws IOException {
 
-        //task01();
+        task01();
         //task02();
-        task03();
+        //task03();
     }
-
-
 
     private static void task01() throws IOException {
         Scanner scanner = new Scanner(System.in);
@@ -21,26 +19,22 @@ public class Homework {
         int moveElements = scanner.nextInt();
         int[] values = new int[length];
 
-        for (int i = 0; i < length; i++){
+        for (int i = 0; i < length; i++) {
             values[i] = scanner.nextInt();
         }
 
         int[] rotatedArray = new int[length];
-        int countOfRotatedArray = 0;
 
-        for (int i = 0; i < values.length; i++){
-            if (i + moveElements < length){
-                rotatedArray[i] = values[moveElements + i];
-            }
-            else{
-                rotatedArray[i] = values[countOfRotatedArray++];
-            }
-            System.out.print(rotatedArray[i] + " ");
+        System.arraycopy(values, 0, rotatedArray, length-moveElements, moveElements);
+        System.arraycopy(values, moveElements, rotatedArray, 0, length-moveElements);
+
+        for (int a : rotatedArray) {
+            System.out.print(a + " ");
         }
+
         scanner.close();
     }
     //=========================
-
 
     private static void task02() throws IOException {
         Scanner scanner = new Scanner(System.in);
@@ -48,16 +42,17 @@ public class Homework {
         int length = scanner.nextInt();
         int[] values = new int[length];
 
-        for (int i = 0; i < length; i++){
+        for (int i = 0; i < length; i++) {
             values[i] = scanner.nextInt();
         }
 
-        for (int i = 0; i < values.length; i++){
+        for (int i = 0; i < values.length; i++) {
             if (values[i] < 0){
                 values[i] = (values[i - 1] + values[i + 1]) / 2;
             }
             System.out.print(values[i] + " ");
         }
+
         scanner.close();
     }
     //===========================
@@ -72,53 +67,24 @@ public class Homework {
             values[i] = scanner.nextInt();
         }
 
-        StringBuilder output = new StringBuilder();
-        int minRange = values[0];
-        int maxRange;
+        System.out.print("[" + values[0]);
 
-        for (int i = 1; i < values.length; i++){
-            if (values[i] - values[i - 1] == 1){
-                if (i == values.length - 1){
-                    maxRange = values[i];
-                    output.append("[" + minRange + " " + maxRange + "]");
-                }
+        for (int i = 1; i < length - 1; i++) {
+            if (values[i-1] + 1 != values[i]) {
+                System.out.print("][" + values[i]);
             }
-            else{
-                if (i == values.length - 1){
-                    maxRange = values[i - 1];
-                    if (minRange == maxRange){
-                        output.append("[" + minRange + "]");
-                    }
-                    else{
-                        output.append("[" + minRange + " " + maxRange + "]");
-                    }
-                    minRange = values[i];
-                    output.append("[" + minRange + "]");
-                }
-                else{
-                    maxRange = values[i - 1];
-                    if (minRange == maxRange){
-                        output.append("[" + minRange + "]");
-                    }
-                    else{
-                        output.append("[" + minRange + " " + maxRange + "]");
-                    }
-                    minRange = values[i];
-                }
+            else if (values[i]+1 != values[i + 1]) {
+                System.out.print(" " + values[i]);
             }
         }
-        System.out.println(output);
+
+        if (values[length-2]+1 != values[length - 1]) {
+            System.out.print("][" + values[length - 1] + "]");
+        }
+        else {
+            System.out.println(" " + values[length - 1] + "]");
+        }
+
         scanner.close();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
